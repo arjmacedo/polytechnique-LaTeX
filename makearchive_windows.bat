ea5aed7f7b13842502cefef7f0363669b73eb766
Alexandre Macedo <alexandre.macedo@vtex.com.br>
2018-07-06 18:38:16 -0300

    Clean up

--- makearchive_windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-7z a -tzip polytechnique-LaTeX.zip makefile_windows.bat makefile_unix.sh README.md source/*.pdf source/*.eps source/polytechnique.sty source/polytechnique.dtx guide/installation.pdf guide/installation.tex guide/logo.pdf
-pause
